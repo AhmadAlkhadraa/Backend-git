77203ea55ee51c9f6ab3ef6b86b979cc086fc73a
Ahmad Alkhadra <A.Alkhadra@eskadenia.com>
2025-08-30 13:17:52 +0300

    first-commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
